1c28980954e4328e4d4fc3fce73c8c533e7766f8
Nguyen Nam Duong <duongpercy410s@gmail.com>
2022-10-19 11:29:55 +0700

    19/10 databiding

--- src/app/component/main/main.component.ts
+++ src/app/component/main/main.component.ts
@@ -3,13 +3,24 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  name: string = 'John';
+  googleURL: string = 'https://www.google.com.vn/';
+  colspan = 2;
+  i: number = 0;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit(): void {}
+  onBtnClick() {
+    console.log('clicked');
   }
-
+  addBtn(event) {
+    console.log(event);
+    this.i = this.i + 1;
+  }
+  substractBtn() {
+    this.i = this.i - 1;
+  }
 }
