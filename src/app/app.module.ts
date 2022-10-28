0f8621d5923d80b4c7f6a4b51bc393ea7880787e
Nguyen Nam Duong <duongpercy410s@gmail.com>
2022-10-28 10:10:06 +0700

    create basic round 28/10

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
+import { HomeComponent } from './components/home/home.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './component/nav-bar/nav-bar.component';
 import { MainComponent } from './component/main/main.component';
 import { FooterComponent } from './component/footer/footer.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
-  declarations: [AppComponent, NavBarComponent, MainComponent, FooterComponent],
-  imports: [BrowserModule, FormsModule, HttpClientModule],
+  declarations: [
+    AppComponent,
+    NavBarComponent,
+    MainComponent,
+    FooterComponent,
+    MoviesComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/home',
+        pathMatch: 'full',
+      },
+      {
+        path: 'home',
+        component: HomeComponent,
+      },
+      { path: 'todos', component: MainComponent },
+      { path: 'movies', component: MoviesComponent },
+    ]),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
